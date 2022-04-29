4b75048ad48120e903ca6655d87ec1d0e77f2996
FHSWar <fhswar0504@163.com>
2022-04-29 23:57:19 +0800

    feat: post-merge 钩子验证完成3

--- script/test2.js
+++ script/test2.js
@@ -1,2 +1,15 @@
+const fs = require('fs');
+
 const str = 'post-script可以在git pull之后触发';
 console.log(str, 'wow');
+
+fs.readFile('./test.js', 'utf8', (err, data) => {
+	if (err) {
+		return console.log(err);
+	}
+	const result = data.replace(/tetstes/g, 'replacement');
+
+	fs.writeFile('./test2.js', result, 'utf8', (e) => {
+		if (e) return console.log(e);
+	});
+});
