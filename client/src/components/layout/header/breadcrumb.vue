adc258500a0ae41b3e839f75a2f65640553443d7
FHSWar <fhswar0504@163.com>
2022-05-05 08:33:58 +0800

    feat: 面包屑变动加了动画，改了tab文案

--- client/src/components/layout/header/breadcrumb.vue
+++ client/src/components/layout/header/breadcrumb.vue
@@ -1,28 +1,36 @@
 <script lang="ts" setup>
 import { ArrowRight } from '@element-plus/icons-vue';
-import { computed } from 'vue';
+import { computed, nextTick, ref, watch } from 'vue';
 import { useStore } from '@/stores';
 import { constantRoutes } from '@/router/constant';
 
 const store = useStore();
 
+const showBreadcrumb = ref(true);
 const breadcrumb = computed(() => {
 	if (store.breadcrumb.length > 0) return store.breadcrumb;
 	return constantRoutes;
+});
+watch(breadcrumb, async () => {
+	showBreadcrumb.value = false;
+	await nextTick();
+	showBreadcrumb.value = true;
 });
 </script>
 
 <template>
   <el-breadcrumb :separator-icon="ArrowRight">
     <template v-for="{icon, id, page, title} in breadcrumb" :key="id">
-      <el-breadcrumb-item v-if="page" :to="{ name: page }">
-        <use-icon v-if="icon" :icon="icon" />
-        {{ title }}
-      </el-breadcrumb-item>
-      <el-breadcrumb-item v-else>
-        <use-icon v-if="icon" :icon="icon" />
-        {{ title }}
-      </el-breadcrumb-item>
+      <transition name="el-fade-in">
+        <el-breadcrumb-item v-if="page" :to="{ name: page }" v-show="showBreadcrumb">
+          <use-icon v-if="icon" :icon="icon" />
+          {{ title }}
+        </el-breadcrumb-item>
+        <el-breadcrumb-item v-else v-show="showBreadcrumb">
+          <use-icon v-if="icon" :icon="icon" />
+          {{ title }}
+        </el-breadcrumb-item>
+      </transition>
     </template>
   </el-breadcrumb>
 </template>
