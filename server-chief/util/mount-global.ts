6f4209b5320164de41ed19f9ad207999d19d64ba
FHSWar <fhswar0504@163.com>
2022-05-09 22:17:28 +0800

    chore: 精修toClient

--- server-chief/util/mount-global.ts
+++ server-chief/util/mount-global.ts
@@ -8,12 +8,12 @@ const toCliect = (
 	ctx: KoaContext,
 	content: any,
 	status:typeof STATUS[keyof typeof STATUS] = STATUS.SUCCESS
-
 ) => {
 	ctx.status = status;
 	if (typeof content === 'string' || content === undefined) {
 		ctx.body = { message: content || '默认返回' };
 	} else {
+		if (!content.message) content.message = '默认返回';
 		ctx.body = content;
 	}
 };
